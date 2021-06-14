a8c0d0f5f83219004c92499f79e25948c8e73d6e
Umut Yüksel <58355025+UmutYuksel@users.noreply.github.com>
2021-06-14 20:37:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace diyetisyen_uygulamasi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new login());  
         }
     }
 }
